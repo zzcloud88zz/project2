6b622f2eb6b917a0f25f25e88a388c7ef62c2e7f
shengrong <shengrong_88@hotmail.com>
2020-12-01 12:11:16 +0800

    project2 to github

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Press Me!"></Button>
       <StatusBar style="auto" />
     </View>
   );

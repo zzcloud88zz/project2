7d16743b97fcbf8e2835fd60a2c749c37868f43d
shengrong <shengrong_88@hotmail.com>
2020-12-01 12:17:35 +0800

    added useState setCount

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
